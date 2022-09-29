986833800ddf494aea89539be2b8ba92b64f47b0
Talal-94 <mito.gldr9@gmail.com>
2022-09-29 16:09:54 +0300

    done

--- src/Component/QuranFetch.jsx
+++ src/Component/QuranFetch.jsx
@@ -3,28 +3,28 @@ import React, { useState, useEffect } from 'react'
 import '../App.css'
 
 function QuranFetch() {
-  const [ Surah, setSurah ] = useState([])
+  const [ Surah, setSurah ] = useState( [] )
   const [ selected, setSelected ] = useState(0)
   const [ isPlaying, setIsPlaying ] = useState(false)
-  const [ Aya, setAya ] = useState([])
-  const [ AudioFile, setAudioFile ] = useState({})
+  const [ Aya, setAya ] = useState( [] )
+  const [ AudioFile, setAudioFile ] = useState( {} )
   const [ lang, setLang ] = useState("en")
   let audio = new Audio()
 
   useEffect(() => {
     axios.get ("http://api.alquran.cloud/v1/surah")
-      .then(( res ) => {
+      .then((res) => {
       setSurah(res.data.data);
     })
 
     fetch(`http://api.alquran.cloud/v1/quran/ar.alafasy`)
-      .then( res => res.json() )
-      .then( data => {
+      .then(res => res.json() )
+      .then(data => {
       setAudioFile(data.data);
     })
   }, [])
 
-  const ayatClick = ( index ) => {
+  const ayatClick = (index) => {
     setSelected(index);
     fetch(`http://api.alquran.cloud/v1/surah/${index}`)
     .then( res => res.json() )
@@ -49,29 +49,29 @@ function QuranFetch() {
     <div className = 'container' >
       <h1 className = 'quran-header'>Quran App</h1>
       <label htmlFor = "text"> Choose a language: </label>
-      <select name = "Language" value = { lang } onChange = {( e )=> setLang(e.target.value)} >
-      <option value = "en"> English </option>
-      <option value = "ar"> عربي </option>
+      <select name = "Language" value = {lang} onChange = {(e)=> setLang(e.target.value)}>
+        <option value = "en"> English </option>
+        <option value = "ar"> عربي </option>
       </select>
       <ul>
-        { Surah.map((item , index) => {
+        {Surah.map((item, index) => {
             return (
               <button key = {index} 
-              onClick = { () => ayatClick(index+1)}>
+              onClick = { () => ayatClick( index + 1 )}>
                 {(lang == "ar") ? item.name : item.englishName}
               </button>)
-          } ) 
-        }
+          })}
       </ul>
+
       <br/>
 
       <div>
       { Aya.map(( aya, i ) => {
         return (
         <div 
-        key = { i } className = 'ayat-text'>  
-        <button className = 'play-btn' onClick = { () => audioClick( selected, i )} >▶️</button>
-        ({ aya.numberInSurah }) - { aya.text } 
+        key = {i} className = 'ayat-text'>  
+        <button className = 'play-btn' onClick = {()=> audioClick( selected, i )}> ▶️ </button>
+        ({ aya.numberInSurah }) - {aya.text} 
         </div>)
       })}
       </div>
